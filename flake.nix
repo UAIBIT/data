7d15d91067e628ffbbe0a9d47fca1dd79a213bc1
juancolchete <juanudkp@gmail.com>
2026-01-02 15:17:42 -0300

    chore: add needed dependency

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Uaibit data development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "uaibit-data";
+
+        buildInputs = with pkgs; [
+          jq
+        ];
+      };
+    };
+}
+
